b19ea6ea6bb6d50a550ea4fa9f490f93c73be6ff
Zabelina Inessa <zab.inessa@yandex.ru>
2023-10-25 12:08:16 +0300

    fix a solution for stat_test

--- JUNIOR/A:B-design/stat_test.py
+++ JUNIOR/A:B-design/stat_test.py
@@ -1,15 +1,13 @@
-from scipy.stats import binom, norm, stats
-from numpy import random
+from typing import Tuple
+from scipy import stats
 import numpy as np
 
 def cpc_sample(
     n_samples: int, conversion_rate: float, reward_avg: float, reward_std: float
 ) -> np.ndarray:
     """Sample data."""
-    
-    cvr = random.binomial(n=1, p=conversion_rate, size=n_samples)
-    cpa = random.norm(loc=reward_avg, scale=reward_std, size=n_samples)
-    
+    cvr = np.random.binomial(n=1, p=conversion_rate, size=n_samples)
+    cpa = np.random.normal(loc=reward_avg, scale=reward_std, size=n_samples)    
     return cvr*cpa
 
 def t_test(cpc_a: np.ndarray, cpc_b: np.ndarray, alpha=0.05
@@ -31,7 +29,6 @@ def t_test(cpc_a: np.ndarray, cpc_b: np.ndarray, alpha=0.05
         True if difference is significant, False otherwise
         p-value
     """
-    
     _, pvalue = stats.ttest_ind(cpc_a, cpc_b)
     accept = bool(pvalue<alpha)
     return (accept, pvalue)
